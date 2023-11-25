82116ca44fe8d5be3d44a1e30c1afa44bc1bf0bc
Darshit Shah <140073108+Darshit152002@users.noreply.github.com>
2023-11-25 14:23:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const randomColor = () => {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 10)];
+  }
+  return color;
+};
+
+console.log(randomColor());
+
+let intervalId;
+
+const startChangingColor = () => {
+  const changeBgColor = () => {
+    document.body.style.backgroundColor = randomColor();
+  };
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 100);
+  }
+};
+
+const stopChangingColor = () => {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
